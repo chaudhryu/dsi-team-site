98d838c7879bda0bd9d8c5e2c2cf972fdebe44a6
chaudhryu <135050470+chaudhryu@users.noreply.github.com>
2025-07-23 15:26:35 -0700

    Beginning Files

---
+++ src/layout/AppFooter.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+
+import { Link } from "react-router";
+import { useSidebar } from "../context/SidebarContext";
+import { ThemeToggleButton } from "../components/common/ThemeToggleButton";
+import NotificationDropdown from "../components/header/NotificationDropdown";
+import UserDropdown from "../components/header/UserDropdown";
+
+const AppFooter: React.FC = () => {
+  const [isApplicationMenuOpen, setApplicationMenuOpen] = useState(false);
+
+  const { isMobileOpen, toggleSidebar, toggleMobileSidebar } = useSidebar();
+
+  const handleToggle = () => {
+    if (window.innerWidth >= 1024) {
+      toggleSidebar();
+    } else {
+      toggleMobileSidebar();
+    }
+  };
+
+  const toggleApplicationMenu = () => {
+    setApplicationMenuOpen(!isApplicationMenuOpen);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if ((event.metaKey || event.ctrlKey) && event.key === "k") {
+        event.preventDefault();
+        inputRef.current?.focus();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  return (
+    <header className="sticky top-0 flex w-full bg-white border-gray-200 z-99999 dark:border-gray-800 dark:bg-gray-900 lg:border-b">
+      <div className="flex flex-col items-center justify-between grow lg:flex-row lg:px-6">
+        <div className="flex items-center justify-between w-full gap-2 px-3 py-3 border-b border-gray-200 dark:border-gray-800 sm:gap-4 lg:justify-normal lg:border-b-0 lg:px-0 lg:py-4">
+        THIS IS THE FOOTER
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default AppFooter;
---
+++ src/layout/AppLayout.tsx
@@ -0,0 +1,45 @@
+import { SidebarProvider, useSidebar } from "../context/SidebarContext";
+import { Outlet } from "react-router";
+import AppHeader from "./AppHeader";
+import Backdrop from "./Backdrop";
+import AppSidebar from "./AppSidebar";
+import AppFooter from "./AppFooter";
+
+const LayoutContent: React.FC = () => {
+  const { isExpanded, isHovered, isMobileOpen } = useSidebar();
+
+  return (
+    <div className="min-h-screen xl:flex">
+      {/* ─────────── Side bar column ─────────── */}
+      <div>
+        <AppSidebar />
+        <Backdrop />
+      </div>
+
+      {/* ─────────── Main column ─────────── */}
+      <div
+        className={`flex flex-col flex-1 min-h-screen transition-all duration-300 ease-in-out
+          ${isExpanded || isHovered ? "lg:ml-[290px]" : "lg:ml-[90px]"}
+          ${isMobileOpen ? "ml-0" : ""}`}
+      >
+        {/* Header (fixed height) */}
+        <AppHeader />
+
+        {/* Scrollable page content */}
+        <div className="flex-1 p-4 md:p-6 mx-auto w-full max-w-[--breakpoint-2xl]">
+          <Outlet />
+        </div>
+
+        <AppFooter />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout: React.FC = () => (
+  <SidebarProvider>
+    <LayoutContent />
+  </SidebarProvider>
+);
+
+export default AppLayout;
