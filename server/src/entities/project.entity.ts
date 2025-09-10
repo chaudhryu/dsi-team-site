7be3ec805059fb558ac52dd3f3b95617a7941d5a
TrungQuanTu <43830733+TrungQuanTu@users.noreply.github.com>
2025-09-10 09:48:15 -0700

    Update project.entity.ts
    keep Joel changes

--- server/src/entities/project.entity.ts
+++ server/src/entities/project.entity.ts
@@ -25,8 +25,17 @@ export class Project {
   @Column()
   status: string;
 
-  @Column({ nullable: true }) // Allow this column to be nu ll
-  githubUrl: string;
+  @Column()
+  client: string;
+
+  @Column({
+    type: "text",
+    transformer: {
+      to: (value: any) => JSON.stringify(value), // array -> string
+      from: (value: string) => JSON.parse(value), // string -> array
+    },
+  })
+  repositories: Repository[];
 
   @ManyToMany(() => User, { eager: true })
   @JoinTable()
