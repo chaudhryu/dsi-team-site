ea75e81cbdc967bc1586b67f69bd6e719f59401e
chaudhryu <135050470+chaudhryu@users.noreply.github.com>
2025-11-04 15:51:52 -0800

    revealing the password
    Revealing the password, and changing icon in sidebar.

--- client/src/layout/AppSidebar.tsx
+++ client/src/layout/AppSidebar.tsx
@@ -7,6 +7,7 @@ import {
   HorizontaLDots,
   ListIcon,
   UserCircleIcon,
+  TableIcon
 } from "../icons";
 import { useSidebar } from "../context/SidebarContext";
 
@@ -29,7 +30,7 @@ const navItems: NavItem[] = [
   },
   { icon: <UserCircleIcon />, name: "User Management", path: "/users" },
   { name: "Projects", icon: <ListIcon />, path: "/projects-internal" },
-  { name: "Databases", icon: <ListIcon />, path: "/databases" },
+  { name: "Databases", icon: <TableIcon />, path: "/databases" },
 
 ];
 
